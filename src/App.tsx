26667f3f32f1595d3a28911719552095bad6e38f
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-09-15 22:39:23 +0300

    added font and created intro section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Stars } from "@react-three/drei";
 import { Earth } from "./components/Earth";
 import { Navbar } from "./components/Navbar";
 import { Intro } from "./pages/intro";
 
 function App() {
   return (
-    <div className="h-screen bg-blue-950">
+    <div className="h-full bg-blue-950">
+      
       <Navbar/>
       <Intro/>
       {/* <Earth /> */}
